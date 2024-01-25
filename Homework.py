d842cba994f5e87faa002714ef9f5eaf5a265bf3
Александр Л <ailomakin@edu.hse.ru>
2024-01-25 22:02:49 +0300

    пше

--- Homework.py
+++ Homework.py
@@ -6,7 +6,9 @@ def square_eq_solver(a, b, c):
 
    if discriminant == 0:
        result.append(-b / (2 * a))
-   elif discriminant > 0:  1
+   elif discriminant > 0:  # <--- изменили условие, теперь
+                           # при нулевом дискриминанте
+                           # не будут вычисляться корни
        result.append((-b + sqrt(discriminant)) / (2 * a))
        result.append((-b - sqrt(discriminant)) / (2 * a))
 
